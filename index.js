eb64d641087b7c2bc8c2fe38bc9ebf01a10da8d3
Ross <ross@mcky.me>
2015-08-05 16:30:45 +0100

    Convert objects to maps and arrays to lists

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 var _ = require('lodash');
 
 module.exports = function(data) {
-	var scss = '';
+    var scss = '';
 
     if (!data) {
-    	return;
+        return;
     }
-    
+
     data = JSON.parse(data);
 
     _.mapKeys(data, function(value, key) {
-		scss+= '$' + key + ': ' + value + ';\r\n'
-	});
+        var isObject = typeof(value) === 'object',
+            isArray = Array.isArray(value);
 
-	return scss;
+        if (isObject || isArray) {
+            scss+= '$' + key + ': (\n';
+            var i = 0;
+
+            _.mapKeys(value, function(value, key, obj) {
+                i++;
+                var len = Object.keys(obj).length,
+                    comma = i !== len ? ',' : '';
+                if (isArray) {
+                    scss+= '\t' + value + comma + '\r\n';
+                } else {
+                    scss+= '\t' + key + ': ' + value + comma + '\r\n';
+                };
+            })
+
+            scss+= ');\n';
+        } else {
+            scss+= '$' + key + ': ' + value + ';\r\n';
+        };
+
+    });
+
+    return scss;
 }
