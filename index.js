38cdffa7e54809361b3d7424c5d141dfc8425dc2
Ross Mackay <ross@mcky.me>
2015-08-07 16:31:57 +0100

    Progress on recursive maps/lists

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
 var _ = require('lodash');
 
+// babelify cli.js?
+
+var indent = function(num, format) {
+    var indentation = '';
+    for (var i = num - 1; i >= 0; i--) {
+        indentation += format || '\t';
+    };
+    return indentation;
+}
+
+var printNested = function(value, key) {
+    var scss = '',
+        level = 1;
+    var printNestedInner = function(key, value) {
+        var scss = '';
+
+        if (_.isObject(value)) {
+            scss += indent(level) + key + ': (\r\n';
+            level++;
+             _.mapKeys(value, function(value, key, obj) {
+                scss += printNestedInner(key, value);
+             })
+            level--;
+            scss += indent(level) + '),\n';
+        } else {
+            var str = isNaN(key) ? key + ': ' : '';
+            scss += indent(level) + str + value + ',\r\n';
+        }
+        return scss;
+    }
+
+    scss+= '$' + key + ': (\n';
+    _.mapKeys(value, function(value, key, obj) {
+        scss += printNestedInner(key, value)
+    })
+    scss += ');\n';
+
+    return scss
+}
+
 module.exports = function(data) {
     var scss = '';
 
@@ -10,25 +50,10 @@ module.exports = function(data) {
     data = JSON.parse(data);
 
     _.mapKeys(data, function(value, key) {
-        var isObject = typeof(value) === 'object',
-            isArray = Array.isArray(value);
-
-        if (isObject || isArray) {
-            scss+= '$' + key + ': (\n';
-            var i = 0;
-
-            _.mapKeys(value, function(value, key, obj) {
-                i++;
-                var len = Object.keys(obj).length,
-                    comma = i !== len ? ',' : '';
-                if (isArray) {
-                    scss+= '\t' + value + comma + '\r\n';
-                } else {
-                    scss+= '\t' + key + ': ' + value + comma + '\r\n';
-                };
-            })
-
-            scss+= ');\n';
+        if (_.isArray(value)) {
+            scss += printNested(value, key)
+        } else if (_.isObject(value)) {
+            scss += printNested(value, key)
         } else {
             scss+= '$' + key + ': ' + value + ';\r\n';
         };
